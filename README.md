d2444fd54201527ac46213605fdd27eaa12fbaf2
AlexitoSnow <ganieves@espol.edu.ec>
2025-09-09 17:05:26 -0500

    build: base project

---
+++ README.md
@@ -0,0 +1,3 @@
+# fclhackathon_team9
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/snow/fclhackathon_team9/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.snow.fclhackathon_team9
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
